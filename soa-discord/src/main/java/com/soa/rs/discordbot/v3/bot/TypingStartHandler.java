02f451c3da5d463954b248683e3f7e426c162dba
Jeff <jeff@soa-rs.com>
2019-12-24 12:09:32 -0500

    Simplify Typing Handler after D4J changes
    "Add member and guild id to start typing event (https://github.com/Discord4J/Discord4J/pull/598)"

--- soa-discord/src/main/java/com/soa/rs/discordbot/v3/bot/TypingStartHandler.java
+++ soa-discord/src/main/java/com/soa/rs/discordbot/v3/bot/TypingStartHandler.java
@@ -3,9 +3,6 @@ package com.soa.rs.discordbot.v3.bot;
 import com.soa.rs.discordbot.v3.usertrack.RecentCache;
 
 import discord4j.core.event.domain.channel.TypingStartEvent;
-import discord4j.core.object.entity.Guild;
-import discord4j.core.object.entity.GuildChannel;
-import discord4j.core.object.util.Snowflake;
 import reactor.core.publisher.Mono;
 
 public class TypingStartHandler {
@@ -13,11 +10,8 @@ public class TypingStartHandler {
 	private RecentCache cache;
 
 	public Mono<Void> handleTypingStart(TypingStartEvent event) {
-		Snowflake channelSnowflake = event.getChannelId();
-		return event.getClient().getGuilds().flatMap(Guild::getChannels)
-				.filter(guildChannel -> guildChannel.getId().equals(channelSnowflake)).flatMap(GuildChannel::getGuild)
-				.flatMap(guild -> Mono.fromRunnable(
-						() -> cache.updateCacheForGuildUser(guild.getId().asLong(), event.getUserId().asLong())))
+		return Mono.fromRunnable(() -> event.getGuildId()
+				.ifPresent(snowflake -> cache.updateCacheForGuildUser(snowflake.asLong(), event.getUserId().asLong())))
 				.then();
 	}
 
